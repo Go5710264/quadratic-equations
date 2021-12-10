a4ee3ead21d8b3a622065e515ae69b9cfe429be7
Dima Shirkov <dimashirkov@list.ru>
2021-12-10 12:05:01 +0300

    Dak1 draft v1.2

--- task.js
+++ task.js
@@ -1,28 +1,30 @@
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   let discriminant = b ** 2 - 4 * a * c; 
   
-  if (discriminant = 0) {
+  if (discriminant === 0) {
 	  let x = -b / (2 * a);
 	 arr.push(x);
   } else if (discriminant > 0) {
-	  let x1 = (-b + Math.sqrt(d)) / (2 * a);
-	  let x2 = (-b - Math.sqrt(d)) / (2 * a);
+	  let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+	  let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 	  arr.push(x1, x2);
-  } else {
-	  console.log('пустой массив')
-  }
+  };
   return arr; // array
 }
 
 // 5,5 500000 5000000 01.01.2027 60months
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount; //Итого
+  let totalAmount = numberMounths * payment; //Итого, общая сумма ипотеки
   let ante = percent * 0.12; // 1/12 процентной ставки !!!
 
-  let payment = amount * (ante + (ante / (((1 + ante) ** 60) - 1))); // ежемесечная оплата
-
+  let returnBank = totalAmount - contribution; // п.3 вернуть банку
+  let payment = amount * (ante + (ante / (((1 + ante) ** numberMounths) - 1))); // ежемесячная оплата
   
+  let currentMonth = new Date().getMonth();
+  let currentYear = new Date().getFullYear();
+  let numberMounths = currentYear * 12 - currentMonth + Date().getMonth();
+
   return totalAmount;
 }
